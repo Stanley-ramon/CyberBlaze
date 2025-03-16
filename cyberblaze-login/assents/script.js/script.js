0651aa877ef647aa9e4c14fefb3ac0e4fc2150a8
Estanlei Sousa <estanlei2020@gmail.com>
2025-03-16 19:12:25 -0300

    adicionando javascript no login

--- cyberblaze-login/assents/script.js/script.js
+++ cyberblaze-login/assents/script.js/script.js
@@ -10,3 +10,47 @@ function togglePassword() {
         eyeIcon.setAttribute('fill', 'white'); // Cor padrão
     }
 }
+
+let Gmail = document.getElementById("email");
+let senha = document.getElementById("senha");
+let button = document.getElementById("button");
+
+function emailEstaVazio() {
+    return Gmail.value.trim() === "";
+}
+
+function senhaEstaVazia() {
+    return senha.value.trim() === "";
+}
+
+function exibirAlerta() {
+    if (emailEstaVazio() && senhaEstaVazia()) {
+        alert("Todos os campos estão em branco! Preencha-os.");
+    } else if (emailEstaVazio()) {
+        alert("O campo Email está em branco! Adicione um Email válido.");
+    } else if (senhaEstaVazia()) {
+        alert("O campo Senha está em branco! Adicione uma Senha válida.");
+    }
+}
+
+function enviarFormulario() {
+    if (!emailEstaVazio() && !senhaEstaVazia()) {
+        alert("Formulário enviado com sucesso!");
+        limparCampos();
+    } else {
+        exibirAlerta();
+    }
+}
+
+function limparCampos() {
+    Gmail.value = "";
+    senha.value = "";
+}
+
+button.addEventListener("click", enviarFormulario);
+
+button.addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        enviarFormulario();
+    }
+})
